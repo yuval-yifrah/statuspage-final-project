4bbda40e0b9eaa4f25308ea2a2652ea636cd0b14
EC2 Default User <ec2-user@ip-10-0-1-134.ec2.internal>
2025-09-08 05:43:41 +0000

    delete all secrets

--- terraform/kubernetes.tf
+++
@@ -1,6 +0,0 @@
-provider "kubernetes" {
-  host                   = aws_eks_cluster.ly_eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.ly_eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
